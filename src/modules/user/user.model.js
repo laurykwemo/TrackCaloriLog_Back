e771d8b310643abdf8fe9c538eec5747232a0ebd
Laury Kwemo <alannbaywala@gmail.com>
2026-07-01 22:08:38 +0200

    Update email sender address in auth controller v3

--- src/modules/user/user.model.js
+++ src/modules/user/user.model.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
         enum: ['user', 'admin'],
         default: 'user'
     },
-    isActive: { type: Boolean, default: true },
+    isActive: { type: Boolean, default: false },
     
     // AUTHENTIFICATION
     isEmailVerified: {
